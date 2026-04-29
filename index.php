725f2bd0b4fa935ea882854888b238ef40b01306
JC <jeancparis@hotmail.com>
2026-04-29 17:44:19 +0200

    workflow test

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 echo "<h1>Bienvenue !</h1>";
-echo "<p>Heureux de vous voir ici. Le site est bientôt prêt... 4 real</p>";
+echo "<p>Heureux de vous voir ici. Le site est bientôt prêt... 4 real 4 real</p>";
 ?>
